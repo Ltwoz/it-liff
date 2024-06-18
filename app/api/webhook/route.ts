f1f22760a09d0e39cbbc802592a9cd14966773f5
Warrapat <warrapat.102@gmail.com>
2024-06-18 14:55:20 +0700

    initial commit

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,42 @@
+import { WebhookRequestBody, messagingApi } from "@line/bot-sdk";
+
+export async function POST(req: Request & { body: WebhookRequestBody }) {
+  const { events } = req.body;
+  const { MessagingApiClient } = messagingApi;
+
+  const client = new MessagingApiClient({
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN || "",
+  });
+
+  try {
+    for (const event of events) {
+      if (event.type === "message" && event.message.type === "text") {
+        const message = event.message.text.toLowerCase();
+
+        let replyMessage = "";
+
+        switch (message) {
+          case "/ตารางเรียน":
+            replyMessage = "ส่งตารางเรียน";
+            break;
+          default:
+            break;
+        }
+
+        client.replyMessage({
+          replyToken: event.replyToken,
+          messages: [
+            {
+              type: "text",
+              text: replyMessage,
+            },
+          ],
+        });
+      }
+    }
+
+    return Response.json({ status: 200 });
+  } catch (error) {
+    throw new Error(`Caught error at webhook with these error : ${error}`);
+  }
+}
