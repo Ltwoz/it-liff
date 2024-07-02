99e577fe1b1b2d455b1d0bb2e9f2a46960380812
Warrapat <warrapat.102@gmail.com>
2024-07-02 11:17:45 +0700

    feat: create liff provider

---
+++ src/providers/liff-provider.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import {
+  FC,
+  PropsWithChildren,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Liff } from "@line/liff";
+
+const LiffContext = createContext<{
+  liff: Liff | null;
+  liffError: string | null;
+}>({
+  liff: null,
+  liffError: null,
+});
+
+export const useLiff = () => useContext(LiffContext);
+
+export const LiffProvider: FC<PropsWithChildren<{ liffId: string }>> = ({
+  children,
+  liffId,
+}) => {
+  const [liff, setLiff] = useState<Liff | null>(null);
+  const [liffError, setLiffError] = useState<string | null>(null);
+
+  const initLiff = useCallback(async () => {
+    try {
+      const liff = (await import("@line/liff")).default;
+
+      await liff.init({ liffId });
+
+      setLiff(liff);
+
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      }
+    } catch (error) {
+      console.log("LIFF init failed.");
+      setLiffError((error as Error).toString());
+    }
+  }, [liffId]);
+
+  useEffect(() => {
+    console.log("LIFF init start...");
+    initLiff();
+  }, [initLiff]);
+
+  return (
+    <LiffContext.Provider value={{ liff, liffError }}>
+      {children}
+    </LiffContext.Provider>
+  );
+};
